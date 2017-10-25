9551ed9f3f57b04a51129ecfe113af41f58fe66c
malik <malik@10.50.137.96>
2017-10-25 10:08:16 +0200

    init

--- src/textadventure/Monsters.java
+++ src/textadventure/Monsters.java
@@ -5,7 +5,8 @@ public class Monsters
     String description, backgroundStory;
     int height, width;
 
-    public Monsters(String description, String backgroundStory, int height, int width) {
+    public Monsters(String description, String backgroundStory, int height, int width) 
+    {
         this.description = description;
         this.backgroundStory = backgroundStory;
         this.height = height;
